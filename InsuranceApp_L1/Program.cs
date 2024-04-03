80499c97036a095ab2840392a9def97be6edb075
Tamas Madarasz-Smith <tamamada@stjohns.school.nz>
2024-04-04 09:09:17 +1300

    All needed coding compleated

--- InsuranceApp_L1/Program.cs
+++ InsuranceApp_L1/Program.cs
@@ -1,17 +1,127 @@
-﻿namespace InsuranceApp_L1
+﻿//Using
+using System.Numerics;
+
+namespace InsuranceApp_L1
 {
     internal class Program
     {
+        //Global Varibles
+        static float totalInsuranceCost = 0f;
+
+        //Methods and Functions
+        static void OneDevice()
+        {
+            List<string> catagory = new List<string>() { "1. Computer", "2. Tablet", "3. Other" };
+
+            //Collect information
+            Console.WriteLine("Enter the device name: \n");
+            string deviceName = Console.ReadLine();
+
+            Console.WriteLine("Enter the number of devices: \n");
+            float deviceAmount = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the device cost for 1 device: \n");
+            float deviceCost = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose the catagory of the device:");
+            for (int index = 0; index < catagory.Count; index++)
+            {
+                Console.WriteLine(catagory[index]);
+            }
+
+            Console.WriteLine(" ");
+
+            int deviceCatagory = CheckInt(1, catagory.Count);
+            //Stop collecting information
+
+            if (deviceAmount > 5)
+            {
+                float insuranceCost = (5 * deviceCost) + (((deviceAmount - 5) * deviceCost) * .9f);
+                Console.Clear();
+                Console.WriteLine($"{deviceName}\n" +
+                               $"Total cost for {deviceAmount} x {deviceName} devices is = ${insuranceCost}");
+                totalInsuranceCost += insuranceCost;
+            }
+            else
+            {
+                float insuranceCost = deviceAmount * deviceCost;
+                Console.Clear();
+                Console.WriteLine($"{deviceName}\n" +
+                              $"Total cost for {deviceAmount} x {deviceName} devices is = ${insuranceCost}");
+                totalInsuranceCost += insuranceCost;
+            }
+            
+            Console.WriteLine("Month: | Value loss:");
+
+            List<float> valueLoss = new List<float>() { deviceCost };
+
+            for (int month = 1; month < 7; month++)
+            {
+                valueLoss.Add((float)Convert.ToDouble(valueLoss[month -1] * 0.95));
+                Console.WriteLine($"{month}      ${Math.Round(valueLoss[valueLoss.Count - 1], 2)}");
+            }
+
+            Console.WriteLine($"Catagory: {catagory[deviceCatagory-1]}");
+        }
+        static string CheckProceed()
+        {
+            while (true)
+            {
+                Console.WriteLine("Press <ENTER> to add another device or type 'X' to exit");
+                string checkProceed = Console.ReadLine();
+
+                checkProceed = checkProceed.ToUpper();
+
+                if (checkProceed.Equals("") || checkProceed.Equals("X") || checkProceed.Equals("x"))
+                {
+                    return checkProceed;
+                }
+
+                DisplayErrorMessage("Error: Invalid choice");
+            }
+        }
+        static void DisplayErrorMessage(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static int CheckInt(int min, int max)
+        {
+            while (true)
+            {
+                try
+                {
+                    int userInt = Convert.ToInt32(Console.ReadLine());
+
+                    if (userInt >= min && userInt <= max)
+                    {
+                        return userInt;
+                    }
+
+                    DisplayErrorMessage($"ERROR: You MUST enter an integer between {min} and {max}.");
+                }
+                catch
+                {
+                    DisplayErrorMessage($"ERROR: You MUST enter an integer between {min} and {max}.");
+                }
+            }
+        }
+
+        //Main or when run...
         static void Main(string[] args)
         {
-            Console.WriteLine("  _____                                                                           \n" +
-                              " |_   _|                                              /\\                         \n" +
-                                "   | |  _ __  ___ _   _ _ __ __ _ _ __   ___ ___     /  \\   _ __  _ __         \n" +
-                                "   | | | '_ \\/ __| | | | '__/ _` | '_ \\ / __/ _ \\   / /\\ \\ | '_ \\| '_ \\  \n" +
-                                "  _| |_| | | \\__ \\ |_| | | | (_| | | | | (_|  __/  / ____ \\| |_) | |_) |     \n" +
-                                " |_____|_| |_|___/\\__,_|_|  \\__,_|_| |_|\\___\\___| /_/    \\_\\ .__/| .__/   \n" +
-                                "                                                           | |   | |            \n" +
-                                "                                                           |_|   |_|              ");
+            string proceed = "";
+            while (proceed.Equals(""))
+            {
+                OneDevice();
+
+                proceed = CheckProceed();
+
+                Console.Clear();
+            }
+
+            Console.WriteLine($"${totalInsuranceCost}");
         }
     }
 }
